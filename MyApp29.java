b655975688f4738aebd245aeaa5d225a3bfff798
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-11 08:58:32 +0900

    29

---
+++ MyApp29.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MyApp29 {
+  public static void main(String[] args) {
+    int[] scores = {70, 90, 80, 60};
+    for (int i = 0; i < scores.length; i++) {
+      System.out.println(scores[i]);
+    }
+  }
+}
