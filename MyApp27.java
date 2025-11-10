f316599740ea325fe9ddd43b04c2f576c0e36dfe
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-11 06:20:48 +0900

    27

---
+++ MyApp27.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MyApp27 {
+  public static void main(String[] args) {
+    System.out.print("Signal? ");
+    String signal = new Scanner(System.in).next();
+    switch (signal) {
+      case "red":
+        System.out.println("Stop");
+        break;
+      case "yellow":
+        System.out.println("Slow down");
+        break;
+      case "blue":
+      case "green":
+        System.out.println("Go");
+        break;
+      default:
+        System.out.println("Wrong signal");
+        break;
+    }
+  }
+}
