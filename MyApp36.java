00d8ebb8b74162daa395d6f7075bcc3bc0ceee96
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-11 09:38:56 +0900

    36

---
+++ MyApp36.java
@@ -0,0 +1,12 @@
+public class MyApp36 {
+  public static void main(String[] args) {
+    // int amount = 120;
+    // if (amount > 0) {
+    //   int sales = 100 * amount;
+    //   System.out.println(sales);
+    // }
+    for (int i = 0; i < 3; i++) {
+      System.out.println(i);
+    }
+  }
+}
