ab4ab4c13aab529378d2f93318c2ea5ec1d9a609
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-11 09:07:07 +0900

    31

---
+++ MyApp31.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MyApp31 {
+  public static void main(String[] args) {
+    double money = 500.0;
+    while (money < 1000.0) {
+      money *= 1.1;
+      System.out.println(money);
+    }
+  }
+}
