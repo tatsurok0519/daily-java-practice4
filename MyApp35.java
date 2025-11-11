60d74c269e56cb2a2fa2c2943cb06a9702a628b5
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-11 09:34:06 +0900

    35

---
+++ MyApp35.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MyApp35 {
+  public static void main(String[] args) {
+    System.out.print("English? ");
+    int english = new Scanner(System.in).nextInt();
+    System.out.print("Math? ");
+    int math = new Scanner(System.in).nextInt();
+    if (english >= 80 && math >= 80) {
+      System.out.println("A!");
+    } else {
+      System.out.println("B!");
+    }
+  }
+}
