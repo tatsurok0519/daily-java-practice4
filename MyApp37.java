199bcb2a320598227f667cb2ea0de1264d8182d5
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-13 10:14:58 +0900

    37

---
+++ MyApp37.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MyApp37 {
+  public static void main(String[] args) {
+    System.out.print("Score? ");
+    int num = new Scanner(System.in).nextInt();
+    System.out.println(num);
+  }
+}
