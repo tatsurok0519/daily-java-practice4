f1553076a56fd126e7b7ab81ae3181551fcf2af3
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-26 11:25:18 +0900

    41

---
+++ MyApp41.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MyApp {
+  public static void main(String[] args) {
+    System.out.print("Score? ");
+    int score = new Scanner(System.in).nextInt();
+    // if (score >= 90) {
+    //   System.out.println("A!");
+    // } else if (score >= 70) {
+    //   System.out.println("B!");
+    // } else {
+    //   System.out.println("C!");
+    // }
+    if (score >= 90) {
+      System.out.println("A!");
+    } else if (score >= 95) {
+      System.out.println("Super!");
+    } else {
+      System.out.println("C!");
+    }
+  }
+}
