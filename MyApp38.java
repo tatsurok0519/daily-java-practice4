c798a887b82cdd8ee865cbbc5563a4f091fcf16d
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-26 11:14:48 +0900

    38-6

--- MyApp38.java
+++ MyApp38.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MyApp38 {
   public static void main(String[] args) {
-    System.out.println("Score?");
+    System.out.print("Score? ");
     int num = new Scanner(System.in).nextInt();
     System.out.println(num);
   }
