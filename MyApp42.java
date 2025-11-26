9714b9c7302a5b0c6b2f9bcc70e6e16537aa750c
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-27 05:12:26 +0900

    42

---
+++ MyApp42.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MyApp {
+  public static void main(String[] args) {
+    System.out.print("Signal? ");
+    String signal = new Scanner(System.in).next();
+    if (signal.equals("red") == true) {
+      System.out.println("Stop");
+    } else {
+      System.out.println("Not red");
+    }
+  }
+}
