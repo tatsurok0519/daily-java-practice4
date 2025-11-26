7ca60fdeeb27e7a5ed50ccac0cfceb3935101fe2
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-27 05:41:04 +0900

    44-5

--- MyApp44.java
+++ MyApp44.java
@@ -7,7 +7,7 @@ public class MyApp {
     // System.out.println(scores[1]);
     // System.out.println(scores[2]);
     for (int i = 0; i < 3; i++) {
-      
+      System.out.println(scores[i]);
     }
   }
 }
