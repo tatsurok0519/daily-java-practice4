f34800f2ae43dd86450fe7a9463dad5a8f198beb
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-27 05:33:13 +0900

    43-5

--- MyApp43.java
+++ MyApp43.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MyApp {
   public static void main(String[] args) {
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 10; i++) {
       System.out.println(i + " Hello");
     }
   }
