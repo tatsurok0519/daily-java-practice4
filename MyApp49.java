8294e57629908a5accddacbf3c9377135e4461b8
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-28 06:03:58 +0900

    49-4

--- MyApp49.java
+++ MyApp49.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class MyApp {
   public static void main(String[] args) {
-    int score = new Scanner(System.in).newtInt();
+    int score = new Scanner(System.in).nextInt();
     String result = score >= 90 ? "A!" : "B!";
+    System.out.println(result);
   }
 }
