98010ac20bf0599764683127eda2984cdb3d7aa5
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-28 05:39:42 +0900

    46-4

--- MyApp46.java
+++ MyApp46.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class MyApp {
   public static void main(String[] args) {
-    int money = 500;
-    while (money < 1000) {
+    double money = 500.0;
+    while (money < 1000.0) {
       money *= 1.1;
+      System.out.println(money);
     }
   }
 }
