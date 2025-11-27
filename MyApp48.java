161073e7bdf761725ea6307a6aee33ddd93673f0
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-28 05:54:29 +0900

    48-5

--- MyApp48.java
+++ MyApp48.java
@@ -4,6 +4,9 @@ public class MyApp {
   public static void main(String[] args) {
     int[] scores = {70, -10, 80, 90};
     for (int score: scores) {
+      if (score < 0) {
+        break;
+      }
       System.out.println(score);
     }
     System.out.println("Finished");
