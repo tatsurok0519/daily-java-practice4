4aeea87a8ade2a3bd747076f78367079287b96da
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-28 05:53:00 +0900

    48-4

--- MyApp48.java
+++ MyApp48.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MyApp {
   public static void main(String[] args) {
-    int[] scores = {70, 80, 90};
+    int[] scores = {70, -10, 80, 90};
     for (int score: scores) {
       System.out.println(score);
     }
