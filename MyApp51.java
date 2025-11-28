d7d58c27c66d4ab401b4e2098104e7548086de12
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-29 06:01:41 +0900

    51

---
+++ MyApp51.java
@@ -0,0 +1,9 @@
+public class MyApp {
+  public static void main(String[] args) {
+    int amount = 120;
+    if (amount > 0) {
+      int sales = 100 * amount;
+      System.out.println(sales);
+    }
+  }
+}
