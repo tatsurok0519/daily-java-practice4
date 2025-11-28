9e6621775a6f499645a0a8786ae81fb7860c586d
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-29 05:57:07 +0900

    50-5

--- MyApp50.java
+++ MyApp50.java
@@ -6,7 +6,7 @@ public class MyApp {
     int english = new Scanner(System.in).nextInt();
     System.out.print("Math? ");
     int math = new Scanner(System.in).nextInt();
-    if (english >= 80 && math >= 80) {
+    if (!(english >= 80 && math >= 80)) {
       System.out.println("A!");
     } else {
       System.out.println("B!");
