e14f1712d2f62871e3b8096e5560e348855d697f
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-30 06:04:36 +0900

    56-3

--- MyApp56.java
+++ MyApp56.java
@@ -1,5 +1,6 @@
 public class MyApp {
   public static void main(String[] args) {
-    System.out.println(isDividable());
+    System.out.println(isDividable(10, 2));
+    System.out.println(isDividable(10, 3));
   }
 }
