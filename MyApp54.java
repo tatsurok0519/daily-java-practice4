2e5da27c763609dd56d1b54def9c640db07d7943
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-30 05:50:36 +0900

    54-4

--- MyApp54.java
+++ MyApp54.java
@@ -3,6 +3,10 @@ public class MyApp {
     return a + b;
   }
 
+  private static void showSum(int a, int b) {
+    System.out.println(a + b);
+  }
+
 
   public static void main(String[] args) {
     System.out.println(sum(3,7));
