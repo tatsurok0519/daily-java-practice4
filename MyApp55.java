627081f3acbe932bbd41229e6a79b7060b5e8962
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-30 05:57:38 +0900

    55-3

--- MyApp55.java
+++ MyApp55.java
@@ -1,8 +1,13 @@
 public class MyApp {
-  public static void main(String[] args) {
+  showAd() {
     System.out.println("---------");
     System.out.println("SALE! 50% OFF!");
     System.out.println("---------");
+  }
+
+
+  public static void main(String[] args) {
+
     System.out.println("BREAKING NEWS!");
     System.out.println("Two baby pandas born at our Zoo!");
     System.out.println("---------");
