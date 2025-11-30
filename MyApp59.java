919093f8308d8467808cac9f6bf428dce891d597
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-12-01 03:17:23 +0900

    59-3

--- MyApp59.java
+++ MyApp59.java
@@ -5,7 +5,14 @@ public class MyApp {
     System.out.println(n);  
   }
 
+  private static void showThreeTimes(double n) {
+    System.out.println(n);  
+    System.out.println(n);  
+    System.out.println(n);  
+  }
+
   public static void main(String[] args) {
     showThreeTimes(3);
+    showThreeTimes(5.2);
   }
 }
