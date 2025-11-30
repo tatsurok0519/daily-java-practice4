068b73c6b2601e203f821c52da982c43ab7f5a72
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-12-01 03:18:04 +0900

    60

---
+++ MyApp60.java
@@ -0,0 +1,18 @@
+public class MyApp {
+  private static void showThreeTimes(int n) {
+    System.out.println(n);
+    System.out.println(n);
+    System.out.println(n);
+  }
+
+  private static void showThreeTimes(double n) {
+    System.out.println(n);
+    System.out.println(n);
+    System.out.println(n);
+  }
+
+  public static void main(String[] args) {
+    showThreeTimes(3);
+    showThreeTimes(5.2);
+  }
+}
