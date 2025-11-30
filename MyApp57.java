54a3a30165ad1ea4b6d877301c3065dcced7e79f
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-12-01 02:55:37 +0900

    57

---
+++ MyApp57.java
@@ -0,0 +1,16 @@
+public class MyApp {
+  private static boolean isDividable(int a, int b) {
+    if (a % b == 0) {
+      return true;
+    } else {
+      return false;
+    }
+    // System.out.println("--- end ---");
+  }
+
+  public static void main(String[] args) {
+    System.out.println(isDividable(10, 2)); // true
+    System.out.println(isDividable(10, 3)); // false
+    System.out.println(isDividable(10, 0));
+  }
+}
