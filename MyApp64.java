cef659a561bcb8944b9fc989518f662b518e53c5
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-12-02 06:08:09 +0900

    64

---
+++ MyApp64.java
@@ -0,0 +1,21 @@
+class User {
+  String name;
+  int score;
+
+  User(String name, int score) {
+    this.name = name;
+    this.score = score;
+  }
+}
+
+public class MyApp {
+  public static void main(String[] args) {
+    User user1 = new User("Taro", 70);
+    User user2 = new User("Jiro", 80);
+
+    System.out.println(user1.name);
+    System.out.println(user1.score);
+    System.out.println(user2.name);
+    System.out.println(user2.score);
+  }
+}
