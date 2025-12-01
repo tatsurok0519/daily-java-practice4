6ecfe4788b59c7b2ab3ae6efc5943fa331a1530d
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-12-02 05:49:24 +0900

    62

---
+++ MyApp62.java
@@ -0,0 +1,11 @@
+class User {
+  String name;
+  int score;
+}
+
+public class MyApp {
+  public static void main(String[] args) {
+    User user1 = new User();
+    User user2 = new User();
+  }
+}
